47b86d8702f359652ee520b74da98b382347aebc
Dmitry Duev <dmitry.duev@gmail.com>
2022-04-08 14:48:34 -0700

    upd

--- functional_tests/login/03-login-cloud-local-key.py
+++ functional_tests/login/03-login-cloud-local-key.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 """WB-8618: warn the user if they are using a local key to log in to cloud"""
+<<<<<<< HEAD
 
 import random
 import string
+=======
+>>>>>>> 272ce0aed8610df9e012b4645d00bac96d90d188
 
 import pytest
 import wandb
@@ -17,10 +20,18 @@ def random_key():
 if __name__ == "__main__":
     # api_key starts with "local", but base_url points to cloud
     with pytest.raises(wandb.errors.UsageError) as e:
+<<<<<<< HEAD
         wandb.login(key=f"local-{random_key()}")
+=======
+        wandb.login(key="local-87eLxjoRhY6u2ofg63NAJo7rVYHZo4NGACOvpSsF}")
+>>>>>>> 272ce0aed8610df9e012b4645d00bac96d90d188
         assert (
             "Attempting to use a local API key to connect to https://api.wandb.ai" in str(e.value)
         )
 
     # check that this logic does not apply if base_url is not cloud
+<<<<<<< HEAD
     assert wandb.login(key=f"local-{random_key()}", host="https://api.wandb.test")
+=======
+    assert wandb.login(key="local-87eLxjoRhY6u2ofg63NAJo7rVYHZo4NGACOvpSsF", host="https://api.wandb.test")
+>>>>>>> 272ce0aed8610df9e012b4645d00bac96d90d188
