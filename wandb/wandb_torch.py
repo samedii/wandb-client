e8c19e0cdb95cf240145bb29cd753df1a2765f34
Katia <katia@wandb.com>
2022-04-11 11:13:59 -0700

    move hook back

--- wandb/wandb_torch.py
+++ wandb/wandb_torch.py
@@ -144,29 +144,6 @@ class TorchHistory:
                     parameter, prefix + name, log_track_grad
                 )
 
-    def _hook_variable_gradient_stats(self, var, name, log_track):
-        """Logs a Variable's gradient's distribution statistics next time backward()
-        is called on it.
-        """
-        if not isinstance(var, torch.autograd.Variable):
-            cls = type(var)
-            raise TypeError(
-                f"Expected torch.Variable, not {cls.__module__}.{cls.__name__}"
-            )
-
-        handle = self._hook_handles.get(name)
-        if handle is not None and self._torch_hook_handle_is_valid(handle):
-            raise ValueError(f'A hook has already been set under name "{name}"')
-
-        def _callback(grad, log_track):
-            if not log_track_update(log_track):
-                return
-            self.log_tensor_stats(grad.data, name)
-
-        handle = var.register_hook(lambda grad: _callback(grad, log_track))
-        self._hook_handles[name] = handle
-        return handle
-
     def log_tensor_stats(self, tensor, name):
         """Add distribution statistics on a tensor's elements to the current History entry"""
         # TODO Handle the case of duplicate names.
@@ -280,6 +257,29 @@ class TorchHistory:
             {name: wandb.Histogram(np_histogram=(tensor.tolist(), bins.tolist()))},
             commit=False,
         )
+
+    def _hook_variable_gradient_stats(self, var, name, log_track):
+        """Logs a Variable's gradient's distribution statistics next time backward()
+        is called on it.
+        """
+        if not isinstance(var, torch.autograd.Variable):
+            cls = type(var)
+            raise TypeError(
+                f"Expected torch.Variable, not {cls.__module__}.{cls.__name__}"
+            )
+
+        handle = self._hook_handles.get(name)
+        if handle is not None and self._torch_hook_handle_is_valid(handle):
+            raise ValueError(f'A hook has already been set under name "{name}"')
+
+        def _callback(grad, log_track):
+            if not log_track_update(log_track):
+                return
+            self.log_tensor_stats(grad.data, name)
+
+        handle = var.register_hook(lambda grad: _callback(grad, log_track))
+        self._hook_handles[name] = handle
+        return handle
 
     def unhook_all(self):
         for handle in self._hook_handles.values():
